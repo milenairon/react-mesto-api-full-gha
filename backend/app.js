cb6353b6368f38815cd66a17f5479a2c283fb65f
Elena Vasileva <milena-iron@yandex.ru>
2024-01-30 21:45:11 +0700

    feat: добавить логирование запросов и ошибок

--- backend/app.js
+++ backend/app.js
@@ -1,72 +1,77 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 // Если запрос не проходит описанную валидацию,
 // celebrate передаст его дальше - в обработчик ошибки
-const cookieParser = require('cookie-parser');
-const { celebrate, Joi, errors } = require('celebrate');
+const cookieParser = require("cookie-parser");
+const { celebrate, Joi, errors } = require("celebrate");
 // Заголовки безопасности проставляются автоматически(безопасность)
-const helmet = require('helmet');
+const helmet = require("helmet");
 
 const app = express();
 
 const { PORT = 3000 } = process.env;
 
-const mongoose = require('mongoose');
-const auth = require('./middlewares/auth');
-const { login, createUser } = require('./controllers/users');
-const limiter = require('./middlewares/rateLimiter');
-const NotFoundError = require('./errors/NotFoundError');
+const mongoose = require("mongoose");
+const auth = require("./middlewares/auth");
+const { login, createUser } = require("./controllers/users");
+const limiter = require("./middlewares/rateLimiter");
+const NotFoundError = require("./errors/NotFoundError");
+const { requestLogger, errorLogger } = require("./middlewares/logger");
 
-const handleErrors = require('./middlewares/handleErrors');
+const handleErrors = require("./middlewares/handleErrors");
 
 app.use(helmet());
 app.use(cookieParser());
 app.use(limiter);
 
 // Подключаемся к серверу Mongo
-mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
+mongoose.connect("mongodb://127.0.0.1:27017/mestodb");
 
 // Сборка пакетов
 app.use(bodyParser.json()); // для собирания JSON-формата
 app.use(bodyParser.urlencoded({ extended: true })); // для приёма веб-страниц внутри POST-запроса
 
+// подключаем логгер запросов
+app.use(requestLogger);
+
 // Регистрация
 app.post(
-  '/signup',
+  "/signup",
   celebrate({
     body: Joi.object().keys({
       name: Joi.string().min(2).max(30),
       about: Joi.string().min(2).max(30),
       avatar: Joi.string().pattern(
-        /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/,
+        /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/
       ),
       email: Joi.string().email().required(),
       password: Joi.string().required(),
     }),
   }),
-  createUser,
+  createUser
 );
 
 // Аутентификация
 app.post(
-  '/signin',
+  "/signin",
   celebrate({
     body: Joi.object().keys({
       email: Joi.string().email().required(),
       password: Joi.string().required(),
     }),
   }),
-  login,
+  login
 );
 
-app.use('/', auth, require('./routes/users'));
-app.use('/', auth, require('./routes/cards'));
+app.use("/", auth, require("./routes/users"));
+app.use("/", auth, require("./routes/cards"));
 
-app.all('*', (req, res, next) => {
-  next(new NotFoundError('Страница не найдена'));
+app.all("*", (req, res, next) => {
+  next(new NotFoundError("Страница не найдена"));
 });
 
-// app.use(errorLogger); // подключаем логгер ошибок
+// подключаем логгер ошибок
+app.use(errorLogger);
 
 // Обработчик ошибок celebrate
 app.use(errors());
